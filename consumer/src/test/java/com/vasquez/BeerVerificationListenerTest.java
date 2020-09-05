1152145023f1f3b2b3f6b06003b9ba89d6f88422
Santiago Vasquez <svasquez@pivotal.io>
2020-09-05 17:49:26 -0400

    add test for rejected verification

--- consumer/src/test/java/com/vasquez/BeerVerificationListenerTest.java
+++ consumer/src/test/java/com/vasquez/BeerVerificationListenerTest.java
@@ -2,14 +2,11 @@ package com.vasquez;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.StubTrigger;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.Instant;
 
@@ -44,10 +41,28 @@ public class BeerVerificationListenerTest {
 
         assertThat(verification, is(notNullValue()));
 
-        assertThat(verification.getName(), is(equalTo( "santiago vasquez")));
+        assertThat(verification.getName(), is(equalTo("santiago vasquez")));
         assertThat(verification.getIsApproved(), is(equalTo(true)));
         assertThat(verification.getBeersCount(), is(equalTo(7)));
-        assertThat(verification.getCity(), is(equalTo( "medellin")));
+        assertThat(verification.getCity(), is(equalTo("medellin")));
         assertThat(verification.getDateOfBirth(), is(equalTo(Instant.parse("1975-04-01T12:00:00Z"))));
+    }
+
+    @Test
+    public void should_call_beer_verification_service_when_verification_was_rejected() {
+
+        // act
+        this.stubTrigger.trigger("rejected_verification");
+
+        // assert
+        BeerVerification verification = beerVerificationServiceMock.getVerificationInRequest();
+
+        assertThat(verification, is(notNullValue()));
+
+        assertThat(verification.getName(), is(equalTo("sebastian vasquez")));
+        assertThat(verification.getIsApproved(), is(equalTo(false)));
+        assertThat(verification.getBeersCount(), is(equalTo(0)));
+        assertThat(verification.getCity(), is(equalTo("new york")));
+        assertThat(verification.getDateOfBirth(), is(equalTo(Instant.parse("2011-02-15T20:00:00Z"))));
     }
 }
